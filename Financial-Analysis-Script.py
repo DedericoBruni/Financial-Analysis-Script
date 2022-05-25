76b9584584825a99bd0e37f2e4349117b4232ad8
DedericoBruni <104196056+DedericoBruni@users.noreply.github.com>
2022-05-25 10:56:30 +0200

    Create Financial-Analysis-Script.py

---
+++ Financial-Analysis-Script.py
@@ -0,0 +1,143 @@
+import Bruni.Pstd as brn
+from pyfiglet import Figlet
+import time
+
+check = 'y'
+while check == 'y':
+    f = Figlet(font='slant')
+    print(f.renderText('Portfolio Analysis'))
+    print('By Federico Bruni')
+    print()
+    print('Choose an Operation: ')
+    print('1- Portfolio Standard Deviation')
+    print('2- Portfolio Expected Return')
+    print('3- Portfolio Correlation')
+    print('4- Find Best Allocation')
+    print('5- Compound Annual Growth Rate')
+    print()
+    choice = input('')
+    check = True
+    w = []
+    tickers = []
+
+    if choice == '1':
+        N = int(input('Insert The Number Of Stocks: '))
+        capital = int(input('Insert total capital invested: '))
+        i = 0
+        while i < N:
+            ticker = input('Insert Ticker: ').upper()
+            investment = int(input('Investment on the stock(integer): '))
+            tickers.append(ticker)
+            weight = investment/capital
+            w.append(weight)
+            i += 1
+        
+        print()
+        year = int(input('Insert Starting Year: '))
+        month = int(input('Insert Starting Month: '))
+        day = int(input('Insert Starting Day: '))
+        std = brn.portfolio_std(tickers,w,year,month,day)
+        std = round(std*100,4)
+        print()
+        print(std, '%')
+        time.sleep(2)
+    
+    if choice == '2':
+        N = int(input('Insert The Number Of Stocks: '))
+        capital = int(input('Insert total capital invested: '))
+        i = 0
+        while i < N:
+            ticker = input('Insert Ticker: ').upper()
+            investment = int(input('Investment on the stock(integer): '))
+            tickers.append(ticker)
+            weight = investment/capital
+            w.append(weight)
+            i += 1
+        
+        print()
+        year = int(input('Insert Starting Year: '))
+        month = int(input('Insert Starting Month: '))
+        day = int(input('Insert Starting Day: '))
+        ret = brn.portfolio_return(tickers,w,year,month,day)
+        ret = (1+ret)**252 -1
+        ret = round(ret,2)
+        print()
+        print(ret, '% Annualized Expected Return')
+        time.sleep(2)
+
+
+    if choice == '3':
+        N = int(input('Insert The Number Of Stocks: '))
+        i = 0
+        while i < N:
+            ticker = input('Insert Ticker: ').upper()
+            tickers.append(ticker)
+            i += 1
+        
+        print()
+        year = int(input('Insert Starting Year: '))
+        month = int(input('Insert Starting Month: '))
+        day = int(input('Insert Starting Day: '))
+        corr = brn.portfolio_corr(tickers,year,month,day)
+        print()
+        print(corr)
+        print()
+        corr['Mean']=(corr.sum()-1)/(N-1)
+        mean_corr = corr['Mean'].mean() 
+        print(' Mean Correlation is:',mean_corr)
+        time.sleep(2)
+
+
+    if choice == '4':
+        N = int(input('Insert The Number Of Stocks: '))
+        i = 0
+        while i < N:
+            ticker = input('Insert Ticker: ').upper()
+            tickers.append(ticker)
+            i += 1
+        
+        print()
+        year = int(input('Insert Starting Year: '))
+        month = int(input('Insert Starting Month: '))
+        day = int(input('Insert Starting Day: '))
+        iterations = int(input('Number of iterations for Montecarlo Simulation: '))
+        alloc = brn.find_best_allocation(tickers,year,month,day,iterations)
+        i = 0
+        for i in range(len(alloc)):
+            alloc[i] = round(alloc[i],3)
+        print()
+        print('Best allocations: ', alloc)
+        print()
+        time.sleep(2)
+
+    if choice == '5':
+        import datetime as dt
+        import pandas_datareader as pdr
+        ticker = input('Insert Ticker: ').upper()
+        print()
+        Ys = int(input('Insert Starting Year: '))
+        Ms = int(input('Insert Starting Month: '))
+        Ds = int(input('Insert Starting Day: '))
+        print()
+        Yf = input('Insert Ending Year: ')
+        Mf = input('Insert Ending Month: ')
+        Df = input('Insert Ending Day: ')
+        Ys = int(Ys)
+        Ms = int(Ms)
+        Ds = int(Ds)
+        start = dt.datetime(Ys, Ms, Ds)
+        df = pdr.get_data_yahoo(ticker, start)
+        price_s = float(df.iloc[0]['Close'])
+        price_f = float(df.loc[Yf+'-'+Mf+'-'+Df]['Close'])
+        df = df.loc[:Yf+'-'+Mf+'-'+Df]
+        t = float(len(df)/365)
+        cagr = (price_f / price_s)**(1 / t) - 1
+        cagr = round(cagr*100,2)
+        cagr = str(cagr)
+        cagr = cagr+'%'
+        print('CAGR: '+cagr)
+        print()
+        time.sleep(2)
+
+
+    check = input('Want to restart?(y/n) ')
